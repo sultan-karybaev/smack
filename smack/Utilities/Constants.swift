72dabbc8adf5bbb7dce6492b1bc697ccb075a0d0
Sultan Karybaev <sultankarybaev@MacBook-Air-Sultan.local>
2018-09-08 19:49:32 +0600

    Alamofire

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://smack-server-sultankarybaev.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNELVC = "unwindToChannelVC"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
