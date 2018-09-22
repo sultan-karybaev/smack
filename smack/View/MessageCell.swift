2851efcdb60c5093cc200be04f06c3b153504d47
Sultan Karybaev <sultankarybaev@MacBook-Air-Sultan.local>
2018-09-23 01:00:54 +0600

    time stamp

--- smack/View/MessageCell.swift
+++ smack/View/MessageCell.swift
@@ -25,8 +25,22 @@ class MessageCell: UITableViewCell {
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         messageLabel.text = message.message
-        timeStampLabel.text = message.timeStamp
         userNameLabel.text = message.userName
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        //isoDate = isoDate.substring(to: end)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 
 }
